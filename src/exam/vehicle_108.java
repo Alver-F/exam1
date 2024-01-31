486eb7b796a60bfb5c136dc31ec4bec882d5b385
Alver-F <rbca22108.rcca@gmail.com>
2024-01-31 10:33:16 +0530

    Commit

---
+++ src/exam/vehicle_108.java
@@ -0,0 +1,17 @@
+package exam;
+
+public class vehicle_108 {
+	String brand;
+	int speed;
+	vehicle_108(String brand,int speed)
+	{
+		this.brand=brand;
+		this.speed=0;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
